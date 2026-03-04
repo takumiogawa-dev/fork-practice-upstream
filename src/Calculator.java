b2157c3500c8e5e43b5314bb98d736bb02c15a10
TakumiOgawa <takumi.ogawa.54872@gmail.com>
2026-03-04 18:25:45 +0900

    初期バージョン: 基本的な計算機クラス作成

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println(Calculator v1.0);
+        System.out.println(加算結果:  + add(10, 20));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
